8e7e3e7ab092e014fa4b27c8043b6fe936067cd7
github-action-benchmark <github@users.noreply.github.com>
2021-01-05 19:52:59 +0000

    add Rust Benchmark (cargo) benchmark result for 6a0e5f54056d07bb94cee6157baeaea52841f0bf

---
+++ dev/components/plurals/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1609876379129,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "6a0e5f54056d07bb94cee6157baeaea52841f0bf",
+          "message": "Prototype out action",
+          "timestamp": "2021-01-05T13:36:29-06:00",
+          "tree_id": "271e27b7e8b99ec65175d1162cb9924d198615c2",
+          "url": "https://github.com/gregtatum/icu4x/commit/6a0e5f54056d07bb94cee6157baeaea52841f0bf"
+        },
+        "date": 1609876377393,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1301,
+            "range": "± 80",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7425,
+            "range": "± 397",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 121990,
+            "range": "± 7849",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
