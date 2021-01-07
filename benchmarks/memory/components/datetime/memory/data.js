d07dc7d57fb4b397c2d4492e7a4c63c05f870bbe
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 21:49:29 +0000

    add Rust Benchmark (ndjson) benchmark result for 918d2cff72188183790c9fb8d70822600c25da67

---
+++ benchmarks/memory/components/datetime/memory/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610056169497,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "918d2cff72188183790c9fb8d70822600c25da67",
+          "message": "Update path for the benchmark data",
+          "timestamp": "2021-01-07T15:46:51-06:00",
+          "tree_id": "f871e27a92532aeea688d05e5243294f8307a129",
+          "url": "https://github.com/gregtatum/icu4x/commit/918d2cff72188183790c9fb8d70822600c25da67"
+        },
+        "date": 1610056164992,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "example components/datetime/memory",
+            "value": 20281,
+            "unit": "bytes",
+            "biggerIsBetter": false,
+            "range": "± 0",
+            "extra": "heap total"
+          },
+          {
+            "name": "example components/datetime/memory",
+            "value": 9360,
+            "unit": "bytes",
+            "biggerIsBetter": false,
+            "range": "± 0",
+            "extra": "heap at global max"
+          },
+          {
+            "name": "example components/datetime/memory",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false,
+            "range": "± 0",
+            "extra": "heap at end"
+          }
+        ]
+      }
+    ]
+  }
+}
