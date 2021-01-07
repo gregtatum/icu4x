e9a1b3da89e311ee2bd05ec81639b8942a9b73da
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 22:00:35 +0000

    add Heap – components/datetime memory (ndjson) benchmark result for 13146f61e0e99176adba872a78f24ddcd77cdc92

--- benchmarks/memory/components/datetime/memory/data.js
+++ benchmarks/memory/components/datetime/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610056169497,
+  "lastUpdate": 1610056835606,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -48,6 +48,50 @@ window.BENCHMARK_DATA = {
             "biggerIsBetter": false,
             "range": "± 0",
             "extra": "heap at end"
+          }
+        ]
+      }
+    ],
+    "Heap – components/datetime memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "13146f61e0e99176adba872a78f24ddcd77cdc92",
+          "message": "Fine-tune labeling",
+          "timestamp": "2021-01-07T15:58:13-06:00",
+          "tree_id": "ecc5d8dc60310adbb27af0e31abd541607c14e8e",
+          "url": "https://github.com/gregtatum/icu4x/commit/13146f61e0e99176adba872a78f24ddcd77cdc92"
+        },
+        "date": 1610056833799,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "heap total allocations",
+            "value": 20281,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at global memory max",
+            "value": 9360,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at end of program execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
           }
         ]
       }
