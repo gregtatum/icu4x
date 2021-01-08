8ea3ac73023e13a986ab8c187641b9d01f04baf0
github-action-benchmark <github@users.noreply.github.com>
2021-01-08 21:02:39 +0000

    add Heap – components/plurals unread_emails (ndjson) benchmark result for 8c82f51577045a02dead73af8863b5897a9fe1ca

--- benchmarks/memory/components/plurals/unread_emails/data.js
+++ benchmarks/memory/components/plurals/unread_emails/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610134207840,
+  "lastUpdate": 1610139759331,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Heap – components/plurals unread_emails": [
@@ -23,6 +23,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gregtatum/icu4x/commit/8c82f51577045a02dead73af8863b5897a9fe1ca"
         },
         "date": 1610134205698,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "heap total allocations",
+            "value": 12804,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at global memory max",
+            "value": 9029,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at end of program execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "8c82f51577045a02dead73af8863b5897a9fe1ca",
+          "message": "Add dhat as a dev dependency",
+          "timestamp": "2021-01-08T13:26:24-06:00",
+          "tree_id": "ba124938f2a6e9e5f430c6d37e65dae86b465927",
+          "url": "https://github.com/gregtatum/icu4x/commit/8c82f51577045a02dead73af8863b5897a9fe1ca"
+        },
+        "date": 1610139758460,
         "tool": "ndjson",
         "benches": [
           {
