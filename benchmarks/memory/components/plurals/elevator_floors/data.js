08e4d4805bfcd457ff411f5be6e3dc9dfe33534a
github-action-benchmark <github@users.noreply.github.com>
2021-01-08 19:30:01 +0000

    add Heap – components/plurals elevator_floors (ndjson) benchmark result for 8c82f51577045a02dead73af8863b5897a9fe1ca

---
+++ benchmarks/memory/components/plurals/elevator_floors/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610134201832,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Heap – components/plurals elevator_floors": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "8c82f51577045a02dead73af8863b5897a9fe1ca",
+          "message": "Add dhat as a dev dependency",
+          "timestamp": "2021-01-08T13:26:24-06:00",
+          "tree_id": "ba124938f2a6e9e5f430c6d37e65dae86b465927",
+          "url": "https://github.com/gregtatum/icu4x/commit/8c82f51577045a02dead73af8863b5897a9fe1ca"
+        },
+        "date": 1610134199602,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "heap total allocations",
+            "value": 13862,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at global memory max",
+            "value": 9114,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at end of program execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
