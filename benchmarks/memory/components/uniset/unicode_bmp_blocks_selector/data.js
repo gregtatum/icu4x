500ca68a3126c316fdf8346be17d19df8e6aecef
github-action-benchmark <github@users.noreply.github.com>
2021-01-08 21:02:03 +0000

    add Heap – components/uniset unicode_bmp_blocks_selector (ndjson) benchmark result for 8c82f51577045a02dead73af8863b5897a9fe1ca

--- benchmarks/memory/components/uniset/unicode_bmp_blocks_selector/data.js
+++ benchmarks/memory/components/uniset/unicode_bmp_blocks_selector/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610134212907,
+  "lastUpdate": 1610139723384,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Heap – components/uniset unicode_bmp_blocks_selector": [
@@ -149,6 +149,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gregtatum/icu4x/commit/8c82f51577045a02dead73af8863b5897a9fe1ca"
         },
         "date": 1610134196095,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "heap total allocations",
+            "value": 976,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at global memory max",
+            "value": 388,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "heap at end of program execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "8c82f51577045a02dead73af8863b5897a9fe1ca",
+          "message": "Add dhat as a dev dependency",
+          "timestamp": "2021-01-08T13:26:24-06:00",
+          "tree_id": "ba124938f2a6e9e5f430c6d37e65dae86b465927",
+          "url": "https://github.com/gregtatum/icu4x/commit/8c82f51577045a02dead73af8863b5897a9fe1ca"
+        },
+        "date": 1610139722258,
         "tool": "ndjson",
         "benches": [
           {
