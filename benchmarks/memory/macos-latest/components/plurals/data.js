2a0e2dca94b9fa45b7dce53358a186961336bd09
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 22:01:38 +0000

    add Heap – components/plurals (ndjson) benchmark result for af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f

---
+++ benchmarks/memory/macos-latest/components/plurals/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610402498636,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Heap – components/plurals": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f",
+          "message": "Fix matrix command",
+          "timestamp": "2021-01-11T15:54:59-06:00",
+          "tree_id": "948d3dfa51482c39019dd3d29cdf3261f57c3921",
+          "url": "https://github.com/gregtatum/icu4x/commit/af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f"
+        },
+        "date": 1610402497701,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unread_emails – Total Heap Allocations",
+            "value": 12821,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails – Heap at Global Memory Max",
+            "value": 9033,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors – Total Heap Allocations",
+            "value": 13879,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors – Heap at Global Memory Max",
+            "value": 9118,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
