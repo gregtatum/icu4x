888aac55a245af93227ebc1def9500380fe771b4
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 21:59:27 +0000

    add Heap – components/locid (ndjson) benchmark result for af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f

---
+++ benchmarks/memory/macos-latest/components/locid/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610402367212,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Heap – components/locid": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f",
+          "message": "Fix matrix command",
+          "timestamp": "2021-01-11T15:54:59-06:00",
+          "tree_id": "948d3dfa51482c39019dd3d29cdf3261f57c3921",
+          "url": "https://github.com/gregtatum/icu4x/commit/af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f"
+        },
+        "date": 1610402362882,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "syntatically_canonicalize_locales – Total Heap Allocations",
+            "value": 554,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales – Heap at Global Memory Max",
+            "value": 345,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Total Heap Allocations",
+            "value": 1263,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Heap at Global Memory Max",
+            "value": 742,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f",
+          "message": "Fix matrix command",
+          "timestamp": "2021-01-11T15:54:59-06:00",
+          "tree_id": "948d3dfa51482c39019dd3d29cdf3261f57c3921",
+          "url": "https://github.com/gregtatum/icu4x/commit/af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f"
+        },
+        "date": 1610402362882,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "syntatically_canonicalize_locales – Total Heap Allocations",
+            "value": 554,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales – Heap at Global Memory Max",
+            "value": 345,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Total Heap Allocations",
+            "value": 1263,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Heap at Global Memory Max",
+            "value": 742,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
