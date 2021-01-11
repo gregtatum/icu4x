a9c723761d0e68156791bff3c42ee8994f08ce7b
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 21:58:37 +0000

    add Heap – components/datetime (ndjson) benchmark result for af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f

---
+++ benchmarks/memory/ubuntu-latest/components/datetime/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610402317101,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Heap – components/datetime": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f",
+          "message": "Fix matrix command",
+          "timestamp": "2021-01-11T15:54:59-06:00",
+          "tree_id": "948d3dfa51482c39019dd3d29cdf3261f57c3921",
+          "url": "https://github.com/gregtatum/icu4x/commit/af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f"
+        },
+        "date": 1610402315245,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "memory – Total Heap Allocations",
+            "value": 20258,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "memory – Heap at Global Memory Max",
+            "value": 9360,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "memory – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log – Total Heap Allocations",
+            "value": 21778,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log – Heap at Global Memory Max",
+            "value": 9616,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log – Heap at End of Program Execution",
+            "value": 1112,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
