5c27f5756d0285d9a5bf2c3d083ce1552851bb15
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 21:59:20 +0000

    add Heap – components/uniset (ndjson) benchmark result for af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f

---
+++ benchmarks/memory/windows-latest/components/uniset/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610402360115,
+  "repoUrl": "https://github.com/gregtatum/icu4x",
+  "entries": {
+    "Heap – components/uniset": [
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f",
+          "message": "Fix matrix command",
+          "timestamp": "2021-01-11T15:54:59-06:00",
+          "tree_id": "948d3dfa51482c39019dd3d29cdf3261f57c3921",
+          "url": "https://github.com/gregtatum/icu4x/commit/af3b3a15b5d0d8f5ae0d67868dfd9f7cc808c86f"
+        },
+        "date": 1610402355994,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector – Total Heap Allocations",
+            "value": 976,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector – Heap at Global Memory Max",
+            "value": 388,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
