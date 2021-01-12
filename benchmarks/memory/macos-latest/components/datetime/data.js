e5620dcdb599662dc4d06c5ab383c614898c29f6
github-action-benchmark <github@users.noreply.github.com>
2021-01-12 17:13:03 +0000

    add Heap – components/datetime (ndjson) benchmark result for a3570537adb7d074d05ec40263a9b8dd9ba46dcb

--- benchmarks/memory/macos-latest/components/datetime/data.js
+++ benchmarks/memory/macos-latest/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610471371796,
+  "lastUpdate": 1610471583177,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Heap – components/datetime": [
@@ -88,6 +88,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "work_log – Total Heap Allocations",
             "value": 21795,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log – Heap at Global Memory Max",
+            "value": 9620,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log – Heap at End of Program Execution",
+            "value": 1112,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "a3570537adb7d074d05ec40263a9b8dd9ba46dcb",
+          "message": "Add a CI job to collect memory benchmarks\n\nThis uses a custom fork of the benchmarking tool to add support for\nndjson as a tool. This ndjson uses the same format as the internal json\nstructure of the benchmarking tool. This allows for fully customizing\nthe output of the data, and what information is collected.",
+          "timestamp": "2021-01-12T11:01:31-06:00",
+          "tree_id": "2bc7d6590af69e9967155a73ac1eb9ca247119b7",
+          "url": "https://github.com/gregtatum/icu4x/commit/a3570537adb7d074d05ec40263a9b8dd9ba46dcb"
+        },
+        "date": 1610471582332,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "work_log – Total Heap Allocations",
+            "value": 16035,
             "unit": "bytes",
             "biggerIsBetter": false
           },
