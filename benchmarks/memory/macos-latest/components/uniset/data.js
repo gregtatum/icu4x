e0c92a3b17141523ab35e26319f34e4085954487
github-action-benchmark <github@users.noreply.github.com>
2021-01-12 17:13:12 +0000

    add Heap – components/uniset (ndjson) benchmark result for a3570537adb7d074d05ec40263a9b8dd9ba46dcb

--- benchmarks/memory/macos-latest/components/uniset/data.js
+++ benchmarks/memory/macos-latest/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610471456035,
+  "lastUpdate": 1610471592046,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Heap – components/uniset": [
@@ -149,6 +149,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gregtatum/icu4x/commit/d33026561021e8edff3df54839bcf83017cc76bd"
         },
         "date": 1610471455125,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector – Total Heap Allocations",
+            "value": 976,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector – Heap at Global Memory Max",
+            "value": 388,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector – Heap at End of Program Execution",
+            "value": 0,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "a3570537adb7d074d05ec40263a9b8dd9ba46dcb",
+          "message": "Add a CI job to collect memory benchmarks\n\nThis uses a custom fork of the benchmarking tool to add support for\nndjson as a tool. This ndjson uses the same format as the internal json\nstructure of the benchmarking tool. This allows for fully customizing\nthe output of the data, and what information is collected.",
+          "timestamp": "2021-01-12T11:01:31-06:00",
+          "tree_id": "2bc7d6590af69e9967155a73ac1eb9ca247119b7",
+          "url": "https://github.com/gregtatum/icu4x/commit/a3570537adb7d074d05ec40263a9b8dd9ba46dcb"
+        },
+        "date": 1610471580839,
         "tool": "ndjson",
         "benches": [
           {
