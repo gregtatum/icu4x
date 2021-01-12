18a7a22513d51ab99c1c58ce3645d439372f76ce
github-action-benchmark <github@users.noreply.github.com>
2021-01-12 15:44:13 +0000

    add Rust Benchmark (cargo) benchmark result for 4d6bb27c12b1b270ec9dd0890ad1dc02a0c278e9

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609876305116,
+  "lastUpdate": 1610466253177,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 8626,
             "range": "± 151",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "4d6bb27c12b1b270ec9dd0890ad1dc02a0c278e9",
+          "message": "Add a CI job to collect memory benchmarks\n\nThis uses a custom fork of the benchmarking tool to add support for\nndjson as a tool. This ndjson uses the same format as the internal json\nstructure of the benchmarking tool. This allows for fully customizing\nthe output of the data, and what information is collected.",
+          "timestamp": "2021-01-12T09:24:12-06:00",
+          "tree_id": "80bacb8b1b39aa62e3ab4a09db5c9ed6a6ea72bc",
+          "url": "https://github.com/gregtatum/icu4x/commit/4d6bb27c12b1b270ec9dd0890ad1dc02a0c278e9"
+        },
+        "date": 1610466246426,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3832,
+            "range": "± 151",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 7789,
+            "range": "± 320",
             "unit": "ns/iter"
           }
         ]
