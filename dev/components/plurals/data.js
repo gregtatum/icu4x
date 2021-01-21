c11ae3b5a9aed8499d0cb8fb4cb8734125576632
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 19:22:48 +0000

    add Rust Benchmark (cargo) benchmark result for 4d22be9adbb60f82194c5cd0b59abef9d90fa48e

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611179147477,
+  "lastUpdate": 1611256968099,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -251,6 +251,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 115106,
             "range": "± 2919",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "4d22be9adbb60f82194c5cd0b59abef9d90fa48e",
+          "message": "Change the output format",
+          "timestamp": "2021-01-21T13:16:05-06:00",
+          "tree_id": "0c75fd8281b757ec993f9b4a06fc159123202d3e",
+          "url": "https://github.com/gregtatum/icu4x/commit/4d22be9adbb60f82194c5cd0b59abef9d90fa48e"
+        },
+        "date": 1611256967112,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1465,
+            "range": "± 101",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6972,
+            "range": "± 450",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 113037,
+            "range": "± 8102",
             "unit": "ns/iter"
           }
         ]
