5be12b8cd8d813d55e05b78e788263db3d631f92
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 19:21:37 +0000

    add Rust Benchmark (cargo) benchmark result for 4d22be9adbb60f82194c5cd0b59abef9d90fa48e

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611179096316,
+  "lastUpdate": 1611256897708,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -215,6 +215,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 6261,
             "range": "± 111",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "tatum.creative@gmail.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "distinct": true,
+          "id": "4d22be9adbb60f82194c5cd0b59abef9d90fa48e",
+          "message": "Change the output format",
+          "timestamp": "2021-01-21T13:16:05-06:00",
+          "tree_id": "0c75fd8281b757ec993f9b4a06fc159123202d3e",
+          "url": "https://github.com/gregtatum/icu4x/commit/4d22be9adbb60f82194c5cd0b59abef9d90fa48e"
+        },
+        "date": 1611256896705,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3531,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5392,
+            "range": "± 10",
             "unit": "ns/iter"
           }
         ]
