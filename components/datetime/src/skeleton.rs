c67b1628768420a18e9ebb639965e3980e9ab16c
Greg Tatum <tatum.creative@gmail.com>
2021-01-28 08:49:22 -0600

    Create a serializer for the skeleton

--- components/datetime/src/skeleton.rs
+++ components/datetime/src/skeleton.rs
@@ -2,11 +2,80 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/master/LICENSE ).
 
-use crate::fields::{Field, FieldLength, FieldSymbol, LengthError, SymbolError};
+use crate::fields::{self, Field, FieldLength, FieldSymbol, LengthError, SymbolError};
+use serde::{
+    de::{self, Deserialize, Deserializer, Unexpected, Visitor},
+    ser::{self, Serialize},
+};
+use std::fmt;
 use std::{collections::HashSet, convert::TryFrom};
 
-#[derive(Debug)]
-pub struct Skeleton(HashSet<Field>);
+#[derive(Debug, PartialEq)]
+pub struct Skeleton {
+    pub fields: HashSet<Field>,
+}
+
+impl Skeleton {
+    pub fn new() -> Skeleton {
+        Skeleton {
+            fields: HashSet::new(),
+        }
+    }
+}
+
+impl From<FieldSymbol> for char {
+    fn from(symbol: FieldSymbol) -> Self {
+        match symbol {
+            FieldSymbol::Year(year) => match year {
+                fields::Year::Calendar => 'y',
+                fields::Year::WeekOf => 'Y',
+            },
+            FieldSymbol::Month(month) => match month {
+                fields::Month::Format => 'M',
+                fields::Month::StandAlone => 'L',
+            },
+            FieldSymbol::Day(day) => match day {
+                fields::Day::DayOfMonth => 'd',
+                fields::Day::DayOfYear => 'D',
+                fields::Day::DayOfWeekInMonth => 'F',
+                fields::Day::ModifiedJulianDay => 'g',
+            },
+            FieldSymbol::Weekday(weekday) => match weekday {
+                fields::Weekday::Format => 'E',
+                fields::Weekday::Local => 'e',
+                fields::Weekday::StandAlone => 'c',
+            },
+            FieldSymbol::DayPeriod(dayperiod) => match dayperiod {
+                fields::DayPeriod::AmPm => 'a',
+            },
+            FieldSymbol::Hour(hour) => match hour {
+                fields::Hour::H11 => 'K',
+                fields::Hour::H12 => 'h',
+                fields::Hour::H23 => 'H',
+                fields::Hour::H24 => 'k',
+            },
+            FieldSymbol::Minute => 'm',
+            FieldSymbol::Second(second) => match second {
+                fields::Second::Second => 's',
+                fields::Second::FractionalSecond => 'S',
+                fields::Second::Millisecond => 'A',
+            },
+        }
+    }
+}
+
+impl From<&Skeleton> for String {
+    fn from(skeleton: &Skeleton) -> Self {
+        let mut string = String::new();
+        for field in skeleton.fields.iter() {
+            let count = field.length as usize;
+            for _ in 0..count {
+                string.push(char::from(field.symbol));
+            }
+        }
+        string
+    }
+}
 
 #[derive(Debug)]
 pub enum SkeletonError {
@@ -47,6 +116,10 @@ impl TryFrom<&str> for Skeleton {
                     | b'Q'
                     // Zone
                     | b'Z'
+                    // Week of Month (numeric)
+                    | b'W'
+                    // Week of Year (numeric)
+                    | b'w'
                     // "-count-*" and "-alt-variant"
                     | b'-' => {
                         return Err(SkeletonError::UnimplementedField(byte as char))
@@ -79,7 +152,58 @@ impl TryFrom<&str> for Skeleton {
             }
         }
 
-        Ok(Self(set))
+        Ok(Self { fields: set })
+    }
+}
+
+// https://serde.rs/deserialize-struct.html
+impl<'de> Deserialize<'de> for Skeleton {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_string(SkeletonVisitor {})
+    }
+}
+
+struct SkeletonVisitor {}
+
+impl<'de> Visitor<'de> for SkeletonVisitor {
+    type Value = Skeleton;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a valid string skeleton formed from date field symbols: http://www.unicode.org/reports/tr35/tr35-dates.html#Date_Field_Symbol_Table")
+    }
+
+    fn visit_str<E>(self, string: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Skeleton::try_from(string).map_err(|err| {
+            let msg = match err {
+                SkeletonError::FieldLengthTooLong => {
+                    "The skeleton contained a field that was too long."
+                }
+                SkeletonError::SymbolUnknown(_) => {
+                    "The skeleton contained a symbol that was unknown."
+                }
+                SkeletonError::UnimplementedField(_) => {
+                    "The skeleton contain a field that is not implemented yet."
+                }
+            };
+            de::Error::invalid_value(Unexpected::Other(msg), &self)
+        })
+    }
+}
+
+impl Serialize for Skeleton {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        let string = String::from(self);
+
+        serializer.serialize_str(&string)
     }
 }
 
@@ -200,4 +324,27 @@ fn test_skeleton() {
             }
         };
     }
+}
+
+#[test]
+fn test_skeleton_serialization() {
+    let parsed: Skeleton = serde_json::from_str("\"MMMMEEEEd\"").unwrap();
+    let mut expected = Skeleton::new();
+
+    expected.fields.insert(Field {
+        symbol: FieldSymbol::from(fields::Day::DayOfMonth),
+        length: FieldLength::One,
+    });
+    expected.fields.insert(Field {
+        symbol: FieldSymbol::from(fields::Weekday::Format),
+        length: FieldLength::Wide,
+    });
+    expected.fields.insert(Field {
+        symbol: FieldSymbol::from(fields::Month::Format),
+        length: FieldLength::Wide,
+    });
+
+    assert_eq!(parsed, expected);
+
+    assert_eq!(serde_json::to_string(&parsed).unwrap(), r#""dMMMMEEEE""#);
 }
