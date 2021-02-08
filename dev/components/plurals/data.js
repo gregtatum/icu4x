43f5e4a5412c37cc32f46c8ca1b8202886738cbb
github-action-benchmark <github@users.noreply.github.com>
2021-02-08 16:36:52 +0000

    add Rust Benchmark (cargo) benchmark result for cd3d48cd32dbff817b878cff399e97493313945b

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612563928202,
+  "lastUpdate": 1612802212756,
   "repoUrl": "https://github.com/gregtatum/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -335,6 +335,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 97987,
             "range": "± 406",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cd3d48cd32dbff817b878cff399e97493313945b",
+          "message": "Merge pull request #1 from gregtatum/memory-benchmark-upload-fix-testing\n\nMemory benchmark upload fix testing",
+          "timestamp": "2021-02-08T10:28:59-06:00",
+          "tree_id": "1f3a9aeeface922dce854ad4cfebcb96334965a8",
+          "url": "https://github.com/gregtatum/icu4x/commit/cd3d48cd32dbff817b878cff399e97493313945b"
+        },
+        "date": 1612802210864,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1796,
+            "range": "± 74",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 8149,
+            "range": "± 441",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 117795,
+            "range": "± 6368",
             "unit": "ns/iter"
           }
         ]
